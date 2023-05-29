83d2349b0a5f200d1e45ace9a93f7d37f3ebabe9
Saba Oji <sabaoji@gmail.com>
2023-05-28 21:42:05 -0400

    Implement login and logout using cookies

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser');
+
 
 app.set("view engine", "ejs");
 
@@ -10,18 +12,26 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 8);
 }
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -33,6 +43,13 @@ app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
+
+app.get("*", (req, res, next) => {
+  res.locals.username = req.cookies.username; // Pass the username to all views
+  next(); // Call the next middleware
+});
+
+
 
 app.post("/urls", (req, res) => {
   const id = generateRandomString(); // Generate a random id
@@ -57,6 +74,11 @@ app.post("/urls/:id/update", (req, res) => {
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
   res.redirect("/urls");
 });
 
